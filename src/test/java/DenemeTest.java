80e4ef5471d14b16acd12f09c358bd60ea1ed469
karakosenur <karakosenur@gmail.com>
2023-07-14 20:38:17 +0300

    v1

--- src/test/java/DenemeTest.java
+++ src/test/java/DenemeTest.java
@@ -1,5 +1,7 @@
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
@@ -10,20 +12,36 @@ import java.util.concurrent.TimeUnit;
 
 public class DenemeTest {
 
-    AndroidDriver<MobileElement> driver;
+    AndroidDriver<AndroidElement> driver;//Android driverlar için kullanilmasi gereken driver
+    //AppiumDriver<AndroidElement> driver2;//hem andrpid hem ios cihazlar için kullanılabilen driver
+    //AppiumDriver<MobileElement> driver3;//ios icin kullanilir(android cihazlar icin de kullanilabilir)
+
 
     @Test
     public void deneme1() throws MalformedURLException {
+        // kullanici gerekli kurulumlari yapar
+        DesiredCapabilities capabilities=new DesiredCapabilities();//yemek kitabını olusturduk
 
-        DesiredCapabilities capabilities=new DesiredCapabilities();
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
-        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\Nurullah Karaköse\\IdeaProjects\\appium\\Apps\\Calculator_8.4.1 (520193683)_Apkpure.apk");
+        //eger ki kullandıgımız sürüm 6 ve üzeri ise automator 2 kyllanıyoruz
 
-        driver=new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+            // capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\Nurullah Karaköse\\IdeaProjects\\appium\\Apps\\Calculator_8.4.1 (520193683)_Apkpure.apk");
+
+        //app capabilityType bir uygulamayı yüklemek istediğimizde indirdigimiz apk dosyasının path uzantısını vererek o uygulama
+
+        driver= new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        //driver.sendSMS("5555555555","selam");
+
+        // kullanici gerekli kurulumlari yapar
+        // uygulamanin yuklendigini dogrular(isInstalled)
+        // uygulamanin acildigini dogrular
+        // carpma,bolme,toplama,cikarma islemleri yaparak sonuclari dogrular
+        // AC butonuna tiklayarak ana ekrani temizler
 
 
 
